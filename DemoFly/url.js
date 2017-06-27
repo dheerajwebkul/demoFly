099aef515d281da0291f365ca348d47312c52d72
Dheeraj Sharma <dheeraj@webkul.com>
2017-06-27 14:58:30 +0530

    updated for PrestaShop

--- DemoFly/url.js
+++ DemoFly/url.js
@@ -1,10 +1,10 @@
-
-chrome.tabs.executeScript( null, {file: "content_script.js"} );
-
+chrome.tabs.executeScript(null, {file: "content_script.js"});
+var demo_link; // Creating a global variable
 chrome.runtime.onMessage.addListener(function(result) {
-  if (result.length>1)
+  if (result.length > 1) {
+      demo_link = result[0];
       renderStatus(result[0], result[1], result[2]);
-  else if (result.length==1){
+  } else if (result.length == 1){
         document.getElementById('custom_message').innerHTML = result[0];
         toggleMe('capture-menu1', 'none');
         toggleMe('capture-menu2', 'block');
@@ -16,19 +16,51 @@ function toggleMe(element_id, value='none'){
 }
 
 function renderStatus(demo_url, framework, module_title) {
-
-      document.getElementById('10').href = demo_url;
-      document.getElementById('txt_copy').value = demo_url;
-      document.getElementById('module_name').innerHTML = module_title;
-      document.getElementById('odoo_version').innerHTML = framework;
-      toggleMe('capture-menu', 'block');
-      toggleMe('capture-menu1', 'none');
+    document.getElementById('10').href = demo_url;
+    document.getElementById('txt_copy').value = demo_url;
+    document.getElementById('module_name').innerHTML = module_title;
+    document.getElementById('odoo_version').innerHTML = framework;
+    toggleMe('capture-menu', 'block');
+    toggleMe('capture-menu1', 'none');
 }
 
+function getLinkWithLifetime(id) {
+  var lifetime = document.getElementById('lifetime').value;
+  lifetime = lifetime.match(/\d+/)[0]; //Get only numbers from the string
+  var element = document.getElementById(id);
+  // Add 'lifetime' only for prestashopdemo site
+  if (element.href.indexOf('prestashopdemo') !== -1) {
+    if (lifetime == "") {
+        element.href = demo_link+"&lifetime=5";
+    } else {
+        element.href = demo_link+"&lifetime="+lifetime;
+    }
+  }
+}
+
+// When click to see the demo, update lifetime in URL
+var id_demo_link = document.getElementById('10');
+id_demo_link.addEventListener('click', function(e) {
+  getLinkWithLifetime('10');
+});
+
+// Copy demo link
 var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
-copyTextareaBtn.addEventListener('click', function(event) {
-  event.preventDefault();
-  document.getElementById("txt_copy").select();
+copyTextareaBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  // @todo::use getLinkWithLifetime() function
+  var lifetime = document.getElementById('lifetime').value;
+  lifetime = lifetime.match(/\d+/)[0]; //Get only numbers from the string
+  var link_copy = document.getElementById("txt_copy");
+  if (link_copy.value.indexOf('prestashopdemo') !== -1) {
+    if (lifetime == "") {
+      link_copy.value = demo_link+"&lifetime=5";
+    } else {
+      link_copy.value = demo_link+"&lifetime="+lifetime;
+    }
+  } // --> till here
+  var text_copy = document.getElementById("txt_copy");
+  text_copy.select();
   try {
     var successful = document.execCommand('copy');
     var msg = successful ? 'successful' : 'unsuccessful';
--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # DemoFly
-A chrome extension to build and check demo on fly for eCommerce/ERP modules/addons, developed by Webkullians. 
+A chrome extension to build and check demo on fly for eCommerce/ERP modules/addons, developed by Webkullians.
 Webkul Store Link: https://store.webkul.com
 
-An addition to it, you can also build demo for FREE ODOO Addons, placed on ODOO Store.  
-Odoo Store Link: https://www.odoo.com/apps
+An addition to it, you can also build demo for PrestaShop Addons,
+PrestaShop Store Link: https://store.webkul.com/PrestaShop-Extensions.html
 
 STEPS FOR INSTALLATION:
 1) Download this repo.
 2) Extract it
 3) Open Chrome, goto settings of Chrome, find "More Tools> Extensions"
 4) Enable "Developer Mode"
-5) Click on "Load unpacked extension", find DemoFly, and proceed 
-6) Hurray !!! Extension has been installed on your chrome. 
+5) Click on "Load unpacked extension", find DemoFly, and proceed
+6) Hurray !!! Extension has been installed on your chrome.
 
 [OR]
 1) Drag and drop the Demofly.crx file onto the Chrome Extensions page(More Tools> Extensions) to install it.
